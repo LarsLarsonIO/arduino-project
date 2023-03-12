7557dd168f0e950d44b2a135edc8cec2397a282b
LarsLarsonIO <goodwllhntng@gmail.com>
2023-03-12 19:23:45 +0100

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include <Arduino.h>
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup(){
+ 
+}
+
+void loop()
+{
   lcd.init();
   lcd.backlight();
   lcd.setCursor(3, 0);
   lcd.print("Test Text");
   lcd.setCursor(2, 1);
-}
-
-void loop()
-{
-
+  lcd.print("Test Text Line 2");
 }
