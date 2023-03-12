ae237a741e3e192f7f9b6dfe53bc7e066e1a5c0e
LarsLarsonIO <goodwllhntng@gmail.com>
2023-03-12 18:48:31 +0100

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,47 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <LCD.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+#define I2C_ADDR 0x27  // Define I2C Address where the PCF8574A is
+#define BACKLIGHT_PIN 3
+#define En_pin  2
+#define Rw_pin  1
+#define Rs_pin  0
+#define D4_pin  4
+#define D5_pin  5
+#define D6_pin  6
+#define D7_pin  7
 
-void setup() {
+int n = 1;
 
-  Serial.begin(9600);
-  lcd.begin(20,4);
+LiquidCrystal_I2C	lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
+
+void setup()
+{
+  lcd.begin (20,4);
   
-  for(int i = 0; i < 3; i++){
-    lcd.backlight();
-    delay(250);
-    lcd.noBacklight();
-    delay(250);
-  }
-  lcd.backlight();
+// Switch on the backlight
+  lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
+  lcd.setBacklight(HIGH);
+  lcd.home ();                   // go home
+
+  lcd.print("SainSmart I2C tester");  
+  lcd.setCursor ( 0, 1 );        // go to the 2nd line
+  lcd.print("F Malpartida library");
+  lcd.setCursor ( 0, 2 );        // go to the third line
+  lcd.print("Test & demonstration");
+  lcd.setCursor ( 0, 3 );        // go to the fourth line
+  lcd.print("Iteration No: ");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // Backlight on/off every 3 seconds
+  lcd.setCursor (14,3);        // go col 14 of line 3
+  lcd.print(n++,DEC);
+  lcd.setBacklight(LOW);      // Backlight off
+  delay(3000);
+  lcd.setBacklight(HIGH);     // Backlight on
+  delay(3000);
 }
